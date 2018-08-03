d3863ec1dda35c4e5f1676feb77a21ec6f37462d
BlueWind <zero18030@gmail.com>
2018-08-03 19:03:24 +0900

    ウィジェット連携 ・データの連携

--- app/src/main/java/com/example/momonyan/widgettodoapp/MainActivity.kt
+++ app/src/main/java/com/example/momonyan/widgettodoapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RemoteViews
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,38 +14,42 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editText1: EditText
     private lateinit var editText2: EditText
     private lateinit var editText3: EditText
+
+
     private lateinit var button: Button
     private lateinit var sharedPreferences:SharedPreferences
-    private lateinit var   prefsPrivateEditor:SharedPreferences.Editor
+    private lateinit var prefsPrivateEditor:SharedPreferences.Editor
     private var memoString:Array<String> = Array<String>(4,{""})
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editText0= findViewById(R.id.editText)
-        editText1= findViewById(R.id.editText2)
-        editText2= findViewById(R.id.editText3)
-        editText3= findViewById(R.id.editText4)
+        editText0 = findViewById(R.id.editText)
+        editText1 = findViewById(R.id.editText2)
+        editText2 = findViewById(R.id.editText3)
+        editText3 = findViewById(R.id.editText4)
+
+
         button = findViewById(R.id.button)
-        sharedPreferences = getSharedPreferences("Widget", MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences("Widget", Context.MODE_MULTI_PROCESS);
 
         memoString[0] = sharedPreferences.getString("list1", "")
         memoString[1] = sharedPreferences.getString("list2", "")
         memoString[2] = sharedPreferences.getString("list3", "")
         memoString[3] = sharedPreferences.getString("list4", "")
 
-        editText0.setText(memoString[0],TextView.BufferType.NORMAL)
-        editText1.setText(memoString[1],TextView.BufferType.NORMAL)
-        editText2.setText(memoString[2],TextView.BufferType.NORMAL)
-        editText3.setText(memoString[3],TextView.BufferType.NORMAL)
+        editText0.setText(memoString[0], TextView.BufferType.NORMAL)
+        editText1.setText(memoString[1], TextView.BufferType.NORMAL)
+        editText2.setText(memoString[2], TextView.BufferType.NORMAL)
+        editText3.setText(memoString[3], TextView.BufferType.NORMAL)
 
 
         prefsPrivateEditor = sharedPreferences.edit()
         button.setOnClickListener {
-            prefsPrivateEditor.putString("list1",editText0.text.toString())
-            prefsPrivateEditor.putString("list2",editText1.text.toString())
-            prefsPrivateEditor.putString("list3",editText2.text.toString())
-            prefsPrivateEditor.putString("list4",editText3.text.toString())
+            prefsPrivateEditor.putString("list1", editText0.text.toString())
+            prefsPrivateEditor.putString("list2", editText1.text.toString())
+            prefsPrivateEditor.putString("list3", editText2.text.toString())
+            prefsPrivateEditor.putString("list4", editText3.text.toString())
             prefsPrivateEditor.apply()
         }
     }
--- app/src/main/java/com/example/momonyan/widgettodoapp/NewAppWidget.kt
+++ app/src/main/java/com/example/momonyan/widgettodoapp/NewAppWidget.kt
@@ -3,25 +3,36 @@ package com.example.momonyan.widgettodoapp
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.RemoteViews
-
+import android.content.IntentFilter
+import android.os.IBinder
+import android.content.ComponentName
 /**
  * Implementation of App Widget functionality.
  */
 class NewAppWidget : AppWidgetProvider() {
-
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
         val views = RemoteViews(context.packageName, R.layout.new_app_widget)
 
         for (appWidgetId in appWidgetIds) {
+            val views = RemoteViews(context.packageName, R.layout.new_app_widget)
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
 
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
     override fun onEnabled(context: Context) {
         // Enter relevant functionality for when the first widget is created
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_TIME_TICK)
+        context.registerReceiver(this, filter)
     }
 
     override fun onDisabled(context: Context) {
@@ -29,27 +40,22 @@ class NewAppWidget : AppWidgetProvider() {
     }
 
     companion object {
+        lateinit var preferences: SharedPreferences
 
         internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager,
                                      appWidgetId: Int) {
             val views = RemoteViews(context.packageName, R.layout.new_app_widget)
-            var tasks = Array<String>(4,{""})
-            var tasksBoolean = Array<Boolean>(4,{false})
-            val preferences = AppCompatActivity().getSharedPreferences("Widget", AppCompatActivity.MODE_PRIVATE);
-            tasks[0] = preferences.getString("list1", "a")
-            tasks[1] = preferences.getString("list2", "b")
-            tasks[2] = preferences.getString("list3", "c")
-            tasks[3] = preferences.getString("list4", "d")
+            preferences = context.getSharedPreferences("Widget", AppCompatActivity.MODE_MULTI_PROCESS)
 
-            tasksBoolean[0] = preferences.getBoolean("listBoolean1", false)
-            tasksBoolean[1] = preferences.getBoolean("listBoolean2", false)
-            tasksBoolean[2] = preferences.getBoolean("listBoolean3", false)
-            tasksBoolean[3] = preferences.getBoolean("listBoolean4", false)
+            Log.d("VIEWDEB", preferences.getString("list1", ""))
+            Log.d("VIEWDEB", preferences.getString("list2", ""))
+            Log.d("VIEWDEB", preferences.getString("list3", ""))
+            Log.d("VIEWDEB", preferences.getString("list4", ""))
 
-            views.setTextViewText(R.id.textView2,tasks[0])
-            views.setTextViewText(R.id.textView3,tasks[1])
-            views.setTextViewText(R.id.textView4,tasks[2])
-            views.setTextViewText(R.id.textView5,tasks[3])
+            views.setTextViewText(R.id.textView2, preferences.getString("list1", ""))
+            views.setTextViewText(R.id.textView3, preferences.getString("list2", ""))
+            views.setTextViewText(R.id.textView4, preferences.getString("list3", ""))
+            views.setTextViewText(R.id.textView5, preferences.getString("list4", ""))
 
             // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
